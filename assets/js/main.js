b178ba07cd44f111ecbe04e6b3f2b6da9458aff6
davide-rullo <davide.rullo01@gmail.com>
2023-09-14 15:45:14 +0200

    add
    added dynamic contact list

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ createApp({
             contacts: [
                 {
                     name: 'Michele',
-                    avatar: './img/avatar_1.png',
+                    avatar: './assets/img/avatar_1.jpg',
                     visible: true,
                     messages: [
                         {
@@ -27,7 +27,7 @@ createApp({
                 },
                 {
                     name: 'Fabio',
-                    avatar: './img/avatar_2.png',
+                    avatar: './assets/img/avatar_2.jpg',
                     visible: true,
                     messages: [
                         {
@@ -49,7 +49,7 @@ createApp({
                 },
                 {
                     name: 'Samuele',
-                    avatar: './img/avatar_3.png',
+                    avatar: './assets/img/avatar_3.jpg',
                     visible: true,
                     messages: [
                         {
@@ -71,7 +71,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro B.',
-                    avatar: './img/avatar_4.png',
+                    avatar: './assets/img/avatar_4.jpg',
                     visible: true,
                     messages: [
                         {
@@ -88,7 +88,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro L.',
-                    avatar: './img/avatar_5.png',
+                    avatar: './assets/img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -105,7 +105,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.png',
+                    avatar: './assets/img/avatar_6.jpg',
                     visible: true,
                     messages: [
                         {
@@ -127,7 +127,7 @@ createApp({
                 },
                 {
                     name: 'Federico',
-                    avatar: './img/avatar_7.png',
+                    avatar: './assets/img/avatar_7.jpg',
                     visible: true,
                     messages: [
                         {
@@ -144,7 +144,7 @@ createApp({
                 },
                 {
                     name: 'Davide',
-                    avatar: './img/avatar_8.png',
+                    avatar: './assets/img/avatar_8.jpg',
                     visible: true,
                     messages: [
                         {
